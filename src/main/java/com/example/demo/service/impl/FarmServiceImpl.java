e6c2dfb9c3d122c8f43cd3635c012548e6917d74
bala-amypo <bala@amypo.in>
2025-12-19 09:16:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FarmServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FarmServiceImpl.java
@@ -1,30 +1,30 @@
-package com.example.demo.service.impl;
+// package com.example.demo.service.impl;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Farm;
-import com.example.demo.repository.FarmRepository;
-import com.example.demo.service.FarmService;
+// import com.example.demo.entity.Farm;
+// import com.example.demo.repository.FarmRepository;
+// import com.example.demo.service.FarmService;
 
-@Service
-public class FarmServiceImpl implements FarmService{
+// @Service
+// public class FarmServiceImpl implements FarmService{
     
-    private final FarmRepository farmRepository;
+//     private final FarmRepository farmRepository;
 
-    public FarmServiceImpl(FarmRepository farmRepository)
-    {
-        this.farmRepository=farmRepository;
-    }
-    @Override
-    public Farm createFarm(Farm farm){
-        return  farmRepository.save(farm);
-    }
+//     public FarmServiceImpl(FarmRepository farmRepository)
+//     {
+//         this.farmRepository=farmRepository;
+//     }
+//     @Override
+//     public Farm createFarm(Farm farm){
+//         return  farmRepository.save(farm);
+//     }
 
-    @Override
-    public List<Farm> listFarms(){
-        return farmRepository.findAll();
-    }
-}
+//     @Override
+//     public List<Farm> listFarms(){
+//         return farmRepository.findAll();
+//     }
+// }
