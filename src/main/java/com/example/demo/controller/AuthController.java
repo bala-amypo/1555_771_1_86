a78e109b5d95e36d15e1425f5455154243a2c3ce
bala-amypo <bala@amypo.in>
2025-12-22 01:15:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,40 +1,44 @@
-package com.example.demo.controller;
-
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entity.User;
-import com.example.demo.service.UserService;
 
 
-import jakarta.validation.Valid;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
 
-    private final UserService userService;
-    public AuthController(UserService userService){
-        this.userService=userService;
-    }
-    @PostMapping("/register")
-    public User postRegister(@RequestBody User user){
-        return userService.register(user);
-    }
+// package com.example.demo.controller;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @PathVariable String email,String password) {
-        User user = userService.findByEmail(email);
-        if (password.equals(user.getPassword())) {
-            return ResponseEntity.ok(Map.of("Name: ",user.getName(),"Role: ",user.getRole()));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-    }
-}
+// import java.util.Map;
+
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.example.demo.entity.User;
+// import com.example.demo.service.UserService;
+
+
+// import jakarta.validation.Valid;
+
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
+
+//     private final UserService userService;
+//     public AuthController(UserService userService){
+//         this.userService=userService;
+//     }
+//     @PostMapping("/register")
+//     public User postRegister(@RequestBody User user){
+//         return userService.register(user);
+//     }
+
+//     @PostMapping("/login")
+//     public ResponseEntity<?> login(@Valid @PathVariable String email,String password) {
+//         User user = userService.findByEmail(email);
+//         if (password.equals(user.getPassword())) {
+//             return ResponseEntity.ok(Map.of("Name: ",user.getName(),"Role: ",user.getRole()));
+//         }
+//         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+//     }
+// }
