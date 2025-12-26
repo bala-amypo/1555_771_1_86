01a73ba7dd7e4ad9add507e00fb70a17ecc54174
bala-amypo <bala@amypo.in>
2025-12-26 16:01:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank
+    @Column(length = 100)
+    private String name;
+    
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+    
+    @NotBlank
+    private String password;
+    
+    @NotBlank
+    private String role;
+    
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Farm> farms;
+}
