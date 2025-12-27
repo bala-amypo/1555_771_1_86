b3e33fe0ddc34e9d0c789fa1c534afe7f149f194
bala-amypo <bala@amypo.in>
2025-12-27 00:49:55 +0000

    User commit

--- src/main/java/com/example/demo/SuggestionServiceImpl.java
+++ src/main/java/com/example/demo/SuggestionServiceImpl.java
@@ -1,16 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.repository.*;
+import com.example.demo.repository.SuggestionRepository;
+import com.example.demo.service.CatalogService;
+import com.example.demo.service.FarmService;
 
 public class SuggestionServiceImpl
         extends com.example.demo.service.impl.SuggestionServiceImpl {
 
-    public SuggestionServiceImpl(
-            FarmRepository farmRepository,
-            CropRepository cropRepository,
-            FertilizerRepository fertilizerRepository,
-            SuggestionRepository suggestionRepository) {
-
-        super(farmRepository, cropRepository, fertilizerRepository, suggestionRepository);
+    public SuggestionServiceImpl(FarmService farmService,
+                                 CatalogService catalogService,
+                                 SuggestionRepository suggestionRepository) {
+        super(farmService, catalogService, suggestionRepository);
     }
 }
