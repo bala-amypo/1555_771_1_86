0a21487a23a572c4a5ffa2a092988d6c33710558
bala-amypo <bala@amypo.in>
2025-12-28 09:39:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/Farm.java
+++ src/main/java/com/example/demo/entity/Farm.java
@@ -24,15 +24,15 @@ public class Farm {
     // @ManyToOne
     // private User owner;
 
-//     @ManyToOne(fetch = FetchType.LAZY)
-// @JoinColumn(name = "owner_id", nullable = false)
-// @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-// private User owner;
-
-@ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
 @JoinColumn(name = "owner_id", nullable = false)
-@JsonIgnoreProperties({"password"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 private User owner;
+
+// @ManyToOne(fetch = FetchType.EAGER)
+// @JoinColumn(name = "owner_id", nullable = false)
+// @JsonIgnoreProperties({"password"})
+// private User owner;
 
 
 }
